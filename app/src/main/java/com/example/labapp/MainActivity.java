07fe9971863a2d7fd2f1f6ea8ccf98eb0e32e631
mihirh19 <miheerhadvani990@gmail.com>
2023-07-24 11:59:26 +0530

    shared preferences app create

--- app/src/main/java/com/example/labapp/MainActivity.java
+++ app/src/main/java/com/example/labapp/MainActivity.java
@@ -2,13 +2,52 @@ package com.example.labapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText user, pass;
+    Button loginbtn;
+    SharedPreferences pref;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        user = (EditText) findViewById(R.id.editTextText);
+        pass = (EditText) findViewById(R.id.editTextText2);
+
+        loginbtn = (Button) findViewById(R.id.button);
+        pref = getSharedPreferences("user_details", MODE_PRIVATE);
+        intent  = new Intent(MainActivity.this, validateActivity.class);
+        if(pref.contains("username") && pref.contains("password")){
+            startActivity(intent);
+        }
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = user.getText().toString();
+                String password = pass.getText().toString();
+
+               if (username.equals("admin") && password.equals("admin")) {
+                    SharedPreferences.Editor editor = pref.edit();
+                    editor.putString("username", username);
+                    editor.putString("password", password);
+                    editor.commit();
+                   Toast.makeText(getApplicationContext(), "Login Successful", Toast.LENGTH_SHORT).show();
+                    startActivity(intent);
+
+                } else {
+                    Toast.makeText(getApplicationContext(), "Login Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
