6ee0a2e1c8bf870f31e318e5066ba0744d40c434
mihirh19 <miheerhadvani990@gmail.com>
2023-08-22 20:41:45 +0530

    sqlite database

--- app/src/main/java/com/example/labapp/detailsActivity.java
+++ app/src/main/java/com/example/labapp/detailsActivity.java
@@ -13,22 +13,22 @@ import android.widget.SimpleAdapter;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class detailsActivity extends AppCompatActivity {
+public class DetailsActivity extends AppCompatActivity {
     Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_details);21
+        setContentView(R.layout.activity_details);
         DbHandler db = new DbHandler(this);
         ArrayList<HashMap<String, String>> userList = db.GetUsers();
         ListView lv = (ListView) findViewById(R.id.user_list);
-        ListAdapter adapter = new SimpleAdapter(detailsActivity.this, userList, R.layout.list_row,new String[]{"name","designation","location"}, new int[]{R.id.name, R.id.designation, R.id.location});
+        ListAdapter adapter = new SimpleAdapter(DetailsActivity.this, userList, R.layout.list_row,new String[]{"name","designation","location"}, new int[]{R.id.name, R.id.designation, R.id.location});
         lv.setAdapter(adapter);
         Button back = (Button)findViewById(R.id.btnBack);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                intent = new Intent(detailsActivity.this,MainActivity.class);
+                intent = new Intent(DetailsActivity.this,MainActivity.class);
                 startActivity(intent);
             }
         });
